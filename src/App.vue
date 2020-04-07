99f00428dc724627a03ca272244a99d60c18a832
Iris789 <582653045@qq.com>
2020-04-07 14:32:06 +0800

    -first

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <alert type="success" title="这是一段成功提示的信息" @defclick='clickHandle' />
+    <alert @defclick="clickHandle" />
+    <alert type="warning" title="这是一段警告提示的信息" @defclick='clickHandle' />
+    <alert type="error" title="这是一段错误提示的信息" @defclick='clickHandle' />
+  </div>
+</template>
+
+<script>
+import alert from '@/components/alert'
+
+export default {
+  name: 'App',
+  components: {
+    alert
+  },
+  methods: {
+    clickHandle() {
+      console.log('test')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/alert.vue
@@ -0,0 +1,43 @@
+<template>
+    <div role="alert" :class="['el-alert',changeAlert,'is-center','is-light']">
+        <i :class="['el-alert__icon',changeIcon]"></i>
+        <div class="el-alert__content">
+            <slot name="title">
+                <span class="el-alert__title">{{title}}</span>
+            </slot>
+            <i class="el-alert__closebtn el-icon-close" @click="clickHandle"></i>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        type:{
+            type:String,
+            default:'info'
+        },
+        title:{
+            type:String,
+            default:'这是一段消息提示的文字'
+        }
+    },
+    computed:{
+        changeAlert:function(){
+            return 'el-alert--'+this.type;
+        },
+        changeIcon:function(){
+            return 'el-icon-'+this.type;
+        }
+    },
+    methods:{
+        clickHandle(){
+            this.$emit('defclick');
+        }
+    }
+}
+</script>
+
+<style>
+@import "https://unpkg.com/element-ui@2.13.0/lib/theme-chalk/index.css";
+</style>
